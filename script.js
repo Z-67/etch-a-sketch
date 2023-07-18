702bbf27cb4479cc27c972dbc1f135c08c7dfc83
Z-67 <121440746+Z-67@users.noreply.github.com>
2023-07-18 16:45:55 +0100

    16x16 grid added

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.getElementById("gridContainer");
+    for (let i = 0; i < 16; i++) {
+        for (let j = 0; j < 16; j++) {
+            const gridItem = document.createElement("div");
+            gridItem.classList.add("grid-item");
+            container.appendChild(gridItem);
+        }
+    }
+});
