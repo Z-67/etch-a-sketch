f5dec36ea169c7c7d973b58c04e6770e8072b9c8
Z-67 <121440746+Z-67@users.noreply.github.com>
2023-07-18 17:05:41 +0100

    hover effect and color change works

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
     const container = document.getElementById("gridContainer");
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
+    const gridSize = 16;
+
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
             const gridItem = document.createElement("div");
             gridItem.classList.add("grid-item");
             container.appendChild(gridItem);
         }
     }
+
+    // Add event listener to update background color permanently on mouseover
+    container.addEventListener("mouseover", (event) => {
+        if (event.target.classList.contains("grid-item")) {
+            event.target.style.backgroundColor = "#f00"; // Change to red (#f00)
+        }
+    });
 });
+
+  
